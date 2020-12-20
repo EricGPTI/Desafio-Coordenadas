16e7a51ac0d9075efa5c572cf3e13ade14d87ad8
EricGPTI <ericgpti@gmail.com>
2020-12-19 21:11:03 -0300

    endpoint, url, views, classes

--- api/views.py
+++ api/views.py
@@ -1,1 +1,33 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework import generics, mixins, viewsets
+from rest_framework.generics import get_object_or_404
+from .models import Position
+from .classes.moviments import Moviment
+from .serializer import PositionSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['POST'])
+def start(request):
+    if request.method == 'POST':
+        data = {'x': 0,
+                'y': 0,
+                'face': 'Direita',
+                'start': '0,0,90',
+                'last_position': '0,0,90'
+                }
+        Position.objects.update_or_create(data)
+        start_position = Position.objects.all()
+        serializer = PositionSerializer(start_position, many=True)
+        return Response(serializer.data[0])
+
+
+@api_view(['GET'])
+def atual(request):
+    if request.method == 'GET':
+        atual = Position.objects.all()
+        serializer = PositionSerializer(atual, many=True)
+        coordenada_atual = serializer.data[0].get('last_position')
+        return Response(coordenada_atual)
+
